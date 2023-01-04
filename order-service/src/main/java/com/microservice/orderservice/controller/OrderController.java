c68523f53dba7a2341b85bf5abbc0b236efd6027
chunkityip <alex0917lfo@hotmail.com>
2023-01-04 10:23:36 -0500

    Adding order service and inventory-service

---
+++ order-service/src/main/java/com/microservice/orderservice/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.microservice.orderservice.controller;
+
+import com.microservice.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    //Once the order placed success , this method will run to place the order
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order placed successfully";
+    }
+}
---
+++ order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
@@ -0,0 +1,46 @@
+package com.microservice.orderservice.service;
+
+import com.microservice.orderservice.controller.OrderLineItemDto;
+import com.microservice.orderservice.controller.OrderRequest;
+import com.microservice.orderservice.model.Order;
+import com.microservice.orderservice.model.OrderLineItems;
+import com.microservice.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber((UUID.randomUUID().toString()));
+
+        List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItemsList);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItems.getPrice());
+        orderLineItems.setQuantity(orderLineItems.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+
+
+}
